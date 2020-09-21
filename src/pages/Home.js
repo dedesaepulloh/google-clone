a673e8e2c77bbdc511176978e3de7415aaae2dc4
dedesaepulloh <dedesaepulloh77@gmail.com>
2020-09-21 22:49:58 +0700

    set up projects and add routers

---
+++ src/pages/Home.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Home.css';
+import {Link} from "react-router-dom";
+
+const Home = () => {
+    return (
+        <div className="home">
+            <h1>This is HOMEPAGE</h1>
+
+            <div className='home__header'>
+                <div className='home__headerLeft'>
+                    <Link to='/about'>About</Link>
+                    <Link to='/store'>Store</Link>
+                </div>
+                <div className='home__headerRight'>
+                    <Link to='/gmail'>Gmail</Link>
+                    <Link to='/images'>Images</Link>
+                    {/* Icon */}
+                    {/* Avatar */}
+                </div>
+            </div>
+
+            <div className='home__body'>
+
+            </div>
+        </div>
+    )
+}
+
+export default Home
