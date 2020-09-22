7880991edc0089c57f4cb3df344b17660becb800
dedesaepulloh <dedesaepulloh77@gmail.com>
2020-09-22 10:46:34 +0700

    add style in header

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import './Home.css';
 import {Link} from "react-router-dom";
+import AppsIcon from "@material-ui/icons/Apps"
+import { Avatar } from '@material-ui/core';
 
 const Home = () => {
     return (
         <div className="home">
-            <h1>This is HOMEPAGE</h1>
-
             <div className='home__header'>
-                <div className='home__headerLeft'>
-                    <Link to='/about'>About</Link>
-                    <Link to='/store'>Store</Link>
+                <div className="home__headerLeft">
+                    <Link to="/about">About</Link>
+                    <Link to="/store">Store</Link>
                 </div>
-                <div className='home__headerRight'>
-                    <Link to='/gmail'>Gmail</Link>
-                    <Link to='/images'>Images</Link>
-                    {/* Icon */}
-                    {/* Avatar */}
+                <div className="home__headerRight">
+                    <Link to="/gmail">Gmail</Link>
+                    <Link to="/images">Images</Link>
+                    <AppsIcon />
+                    <Avatar />
                 </div>
             </div>
 
-            <div className='home__body'>
+            <div className="home__body">
 
             </div>
         </div>
